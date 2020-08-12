a48412bd2edb134a46432763a03fab3167d1ce84
Jeremy Gager <jeremymgager@aol.com>
2020-08-12 18:24:07 -0400

    added questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,36 @@
 var questions = [
 
     {
-        q; "question one",
+        q1: "How many questions are in this quiz?",
         choices: [
-        "one", "two", "three", "four", "five"
+        "one", "three", "five", "seven", "enough"
         ],
-        answerIndex: 1
+        answer: "enough"
+    },
+    {   
+        q2: "Does it ever rain in Florida?",
+        choices: [
+            "yes", "no", "never", "only saturdays"
+        ],
+        answer: "yes"
+    },
+    {
+        q3: "do you remember the time you remembered the time that you couldn't remember remembering?",
+        choices; [ "yes", "no", "maybe", "remember"
+        ],
+        answer: "remember"
+    },
+    {   
+        q4: "What is the biggest answer?",
+        choices: ["Banana", "potato", "biggest answer", "manicure"
+        ],
+        answer: "biggest answer"
     }
+    {   
+        q5: "Can an ant be an aunt?",
+        choices: ["how old are they", "I never thought of it", "do they have a sister", "panama"
+        ],
+        answer: "I never thought of it"
+    }
+    
 ];
